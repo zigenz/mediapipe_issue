c2ea2318c1061c611d61cf7e81b6d572ae6c90cc
Zyrus Khambatta <zyrus@khambatta.com.au>
2021-05-24 10:34:57 +0800

    working with reference android example

--- mediapipe_aar/generate_aar.sh
+++ mediapipe_aar/generate_aar.sh
@@ -3,9 +3,9 @@
 SOURCE_DIR="${PWD}"
 SOURCE_BUILD_FILE_NAME="BUILD.copy_to_dest"
 TARGET_FOLDER_NAME="mediapipe/examples/android/src/java/com/google/mediapipe/apps/mediapipe_aar"
-TARGET_ROOT_PATH="../third_party/mediapipe"
+TARGET_ROOT_PATH="${PWD}/../third_party/mediapipe"
 TARGET_PATH="${TARGET_ROOT_PATH}/${TARGET_FOLDER_NAME}"
-
+AAR_NAME="speqs_mediapipe_aar"
 
 function clean()
 {
@@ -24,7 +24,7 @@ function mkdir_target()
 
 clean
 
-# make the director
+# make the directory
 mkdir_target
 if [ $? -ne 0 ]; then
     echo "Cannot create target directory ()${TARGET_PATH}!"
@@ -51,8 +51,20 @@ fi
 echo "Switched path to: ${TARGET_PATH}"
 
 # invoke the bazel command
-echo "Calling \"bazel build -c opt --strip=ALWAYS --host_crosstool_top=@bazel_tools//tools/cpp:toolchain --fat_apk_cpu=arm64-v8a,armeabi-v7a //${TARGET_FOLDER_NAME}:speqs_mediapipe_aar.aar\""
-bazel build -c opt --strip=ALWAYS --host_crosstool_top=@bazel_tools//tools/cpp:toolchain --fat_apk_cpu=arm64-v8a,armeabi-v7a //${TARGET_FOLDER_NAME}:speqs_mediapipe_aar.aar
+#echo "Calling \"bazel build -c opt --strip=ALWAYS --host_crosstool_top=@bazel_tools//tools/cpp:toolchain --fat_apk_cpu=arm64-v8a,armeabi-v7a //${TARGET_FOLDER_NAME}:speqs_mediapipe_aar.aar\""
+bazel build -c opt --strip=ALWAYS --host_crosstool_top=@bazel_tools//tools/cpp:toolchain --fat_apk_cpu=arm64-v8a,armeabi-v7a //${TARGET_FOLDER_NAME}:${AAR_NAME}.aar
+if [ $? -ne 0 ]; then
+    echo "Build failed!"
+    clean
+    exit 1;
+fi
 
+# copy to local bin folder
+if !( test -f "${SOURCE_DIR}/bin" ); then
+    mkdir "${SOURCE_DIR}/bin"
+    echo "Created folder: ${SOURCE_DIR}/bin"
+fi
+
+cp "${TARGET_ROOT_PATH}/bazel-bin/${TARGET_FOLDER_NAME}/${AAR_NAME}.aar" "${SOURCE_DIR}/bin/"
 
 exit 0
