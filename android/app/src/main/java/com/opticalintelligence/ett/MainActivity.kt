a54941f8d3c84adae1c496dd943430ccfd28db0c
Zyrus Khambatta <zyrus@khambatta.com.au>
2021-05-25 10:28:12 +0800

    for some strange reason, worked with wrong mainactivity.  adapted to correct one, now.

--- android/app/src/main/java/com/opticalintelligence/ett/MainActivity.kt
+++ android/app/src/main/java/com/opticalintelligence/ett/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.opticalintelligence.ett
 
+import android.graphics.SurfaceTexture
 import android.os.Bundle
-import android.util.Log;
+import android.util.Log
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList
-import com.google.mediapipe.framework.AndroidPacketCreator
 import com.google.mediapipe.framework.Packet
 import com.google.mediapipe.framework.PacketGetter
+import com.google.protobuf.InvalidProtocolBufferException
 import java.util.*
 
-/** Main activity of MediaPipe face mesh app.  */
+
+/** Main activity of MediaPipe iris tracking app.  */
 class MainActivity : MainActivityBase() {
+    private var haveAddedSidePackets = false
+    override fun onCameraStarted(surfaceTexture: SurfaceTexture?) {
+        super.onCameraStarted(surfaceTexture)
+
+        // onCameraStarted gets called each time the activity resumes, but we only want to do this once.
+        if (!haveAddedSidePackets) {
+            val focalLength: Float = cameraHelper.getFocalLengthPixels()
+            if (focalLength != Float.MIN_VALUE) {
+                val focalLengthSidePacket: Packet =
+                    processor.getPacketCreator().createFloat32(focalLength)
+                val inputSidePackets: MutableMap<String, Packet> = HashMap()
+                inputSidePackets[FOCAL_LENGTH_STREAM_NAME] =
+                    focalLengthSidePacket
+                processor.setInputSidePackets(inputSidePackets)
+            }
+            haveAddedSidePackets = true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val packetCreator: AndroidPacketCreator = processor.getPacketCreator()
-        val inputSidePackets: MutableMap<String, Packet> = HashMap()
-        inputSidePackets[INPUT_NUM_FACES_SIDE_PACKET_NAME] = packetCreator.createInt32(NUM_FACES)
-        processor.setInputSidePackets(inputSidePackets)
 
         // To show verbose logging, run:
         // adb shell setprop log.tag.MainActivity VERBOSE
@@ -23,51 +40,52 @@ class MainActivity : MainActivityBase() {
             processor.addPacketCallback(
                 OUTPUT_LANDMARKS_STREAM_NAME
             ) { packet ->
-                Log.v(TAG, "Received multi face landmarks packet.")
-                val multiFaceLandmarks =
-                    PacketGetter.getProtoVector(packet, NormalizedLandmarkList.parser())
-                Log.v(
-                    TAG,
-                    ("[TS:"
-                            + packet.getTimestamp()
-                            ) + "] " + getMultiFaceLandmarksDebugString(
-                        multiFaceLandmarks
+                val landmarksRaw = PacketGetter.getProtoBytes(packet)
+                try {
+                    val landmarks =
+                        NormalizedLandmarkList.parseFrom(landmarksRaw)
+                    if (landmarks == null) {
+                        Log.v(
+                            TAG,
+                            "[TS:" + packet.getTimestamp().toString() + "] No landmarks."
+                        )
+                        return@addPacketCallback
+                    }
+                    Log.v(
+                        TAG,
+                        ("[TS:"
+                                + packet.getTimestamp()
+                                ) + "] #Landmarks for face (including iris): "
+                                + landmarks.landmarkCount
                     )
-                )
+                    Log.v(
+                        TAG,
+                        getLandmarksDebugString(landmarks)
+                    )
+                } catch (e: InvalidProtocolBufferException) {
+                    Log.e(
+                        TAG,
+                        "Couldn't Exception received - $e"
+                    )
+                    return@addPacketCallback
+                }
             }
         }
     }
 
     companion object {
         private const val TAG = "MainActivity"
-        private const val INPUT_NUM_FACES_SIDE_PACKET_NAME = "num_faces"
-        private const val OUTPUT_LANDMARKS_STREAM_NAME = "multi_face_landmarks"
-
-        // Max number of faces to detect/process.
-        private const val NUM_FACES = 1
-        private fun getMultiFaceLandmarksDebugString(
-            multiFaceLandmarks: kotlin.collections.List<NormalizedLandmarkList>
-        ): String {
-            if (multiFaceLandmarks.isEmpty()) {
-                return "No face landmarks"
-            }
-            var multiFaceLandmarksStr = """
-                Number of faces detected: ${multiFaceLandmarks.size}
-                
-                """.trimIndent()
-            var faceIndex = 0
-            for (landmarks in multiFaceLandmarks) {
-                multiFaceLandmarksStr += """	#Face landmarks for face[$faceIndex]: ${landmarks.landmarkCount}
+        private const val FOCAL_LENGTH_STREAM_NAME = "focal_length_pixel"
+        private const val OUTPUT_LANDMARKS_STREAM_NAME = "face_landmarks_with_iris"
+        private fun getLandmarksDebugString(landmarks: NormalizedLandmarkList): String {
+            var landmarkIndex = 0
+            var landmarksString = ""
+            for (landmark in landmarks.landmarkList) {
+                landmarksString += """		Landmark[$landmarkIndex]: (${landmark.x}, ${landmark.y}, ${landmark.z})
 """
-                var landmarkIndex = 0
-                for (landmark in landmarks.landmarkList) {
-                    multiFaceLandmarksStr += """		Landmark [$landmarkIndex]: (${landmark.x}, ${landmark.y}, ${landmark.z})
-"""
-                    ++landmarkIndex
-                }
-                ++faceIndex
+                ++landmarkIndex
             }
-            return multiFaceLandmarksStr
+            return landmarksString
         }
     }
 }
